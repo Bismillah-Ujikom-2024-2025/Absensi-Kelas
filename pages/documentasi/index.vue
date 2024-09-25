c5828673c1cbc1a89883392ed2ec0be5c8e039de
RadiTEFA <radingoding@gmail.com>
2024-09-25 15:01:18 +0700

    Add files via upload

---
+++ pages/documentasi/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+            <div class="card col-mx-2" style="width:200px">
+                <img class="card-img-top" src="/assets/img/profil.jpg" alt="Card image" style="width:100%">
+                <div class="card-body ButtonHapus">
+                    <a href="#" class="btn btn-danger stretched-link ButtonHapus">Delete</a>
+                </div>
+            </div>
+            <div class="card col-2" style="width:200px">
+                <img class="card-img-top" src="/assets/img/profil.jpg" alt="Card image" style="width:100%">
+                <div class="card-body ButtonHapus">
+                    <a href="#" class="btn btn-danger stretched-link ButtonHapus">Delete</a>
+                </div>
+            </div>
+    </div>
+</template>
+
+<style scoped>
+.container{
+    margin: 10%;
+}
+.ButtonHapus{
+    text-align: center;
+}
+</style>
